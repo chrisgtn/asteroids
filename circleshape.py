075957a0930c2725fd28a97f53fda80985b39c9f
chrisgtn <christinagaitanou@gmail.com>
2024-10-10 13:02:50 +0200

    Player

---
+++ circleshape.py
@@ -0,0 +1,31 @@
+import pygame
+
+
+
+"""CircleShape extends the Sprite class to also store a position, velocity, and radius.
+Later you'll write subclasses of CircleShape and override the draw and 
+update methods with the logic for that particular game object."""
+
+
+
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     
@@ -9,12 +10,31 @@ def main():
     # game screen set up
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
     
+    # clock for FPS
+    clock = pygame.time.Clock()
+    dt = 0 # delta time
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    # groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    # Player object
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
+    
+    #instead of containers
+    # updatable.add(player)
+    # drawable.add(player)
+    
+    
     # Game loop
-   
     while True:
         
         # handling events (+ check for quit)
@@ -22,13 +42,19 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        # fill screen RGB
+        dt = clock.tick(60) / 1000 # milliseconds to seconds // fps = 60
         screen.fill((0,0,0))
+        
+        for obj in drawable:
+            obj.draw(screen)
     
         # refresh screen for changes
         pygame.display.flip()
         
+        for obj in updatable:
+            obj.update(dt)
 
+        
 
 
 
